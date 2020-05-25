c370a9f54f3b18b04b7c8f180944c55496b4a5b6
SwethaPuppy <swethanivaasini@gmail.com>
2020-05-25 17:07:38 +0530

    First commit with 2 assignments

---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package guru.springframework.spring5webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+
+/**
+ * @author sweth
+ *
+ * created on May 24, 2020
+ */
+@Component
+public class BootStrapData implements CommandLineRunner{
+
+	private final AuthorRepository authorRepository;
+	private final BookRepository bookRepository;
+	private final PublisherRepository publisherRepository;
+	
+	public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository,PublisherRepository publisherRepository) {
+		this.authorRepository = authorRepository;
+		this.bookRepository = bookRepository;
+		this.publisherRepository = publisherRepository;
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		Author eric = new Author("Eric","Evans");
+		Book book = new Book("Domain Driven Design","12345");
+		eric.getBooks().add(book);
+		book.getAuthors().add(eric);
+		
+		authorRepository.save(eric);
+		bookRepository.save(book);
+		
+		Author rod = new Author("Rod", "Johnson");
+		Book book1 = new Book("J2EE Development without EJB","67890");
+		
+		authorRepository.save(rod);
+		bookRepository.save(book1);
+		
+		Publisher publisher = new Publisher();
+		publisher.setName("James Clear");
+		publisher.setAddress1("9/14 ABC Street");
+		publisher.setCity("Washington DC");
+		publisher.setState("Michigan");
+		
+		publisherRepository.save(publisher);
+		
+		System.out.println("Publisher count:: "+publisherRepository.count());
+		
+		System.out.println("Bootstrap is running");
+		
+		System.out.println("No of books :: "+bookRepository.count());
+		
+	}
+
+}
---
+++ spring5webapp/src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -0,0 +1,108 @@
+package guru.springframework.spring5webapp.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GeneratorType;
+
+@Entity
+public class Publisher {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String name;
+	private String address1;
+	private String city;
+	private String State;
+	private String zip;
+	
+	public Publisher() {
+		
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return State;
+	}
+
+	public void setState(String state) {
+		State = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Publisher other = (Publisher) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Publisher [id=" + id + ", name=" + name + ", address1=" + address1 + ", city=" + city + ", State="
+				+ State + ", zip=" + zip + "]";
+	}
+	
+	
+	
+	}
